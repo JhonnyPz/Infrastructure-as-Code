6bf5d5e966b7fbd7144bfe9141b3dff14ce8cf6c
JhonnyPz <jhonnycano42@gmail.com>
2023-07-15 12:10:03 -0500

    test-finish

--- Deployment/Web-Application/Infrastructure/outputs.tf
+++ Deployment/Web-Application/Infrastructure/outputs.tf
@@ -6,15 +6,15 @@ output "rg-name" {
 
 output "rg-location" {
   value       = azurerm_resource_group.rg.location
-  description = "Ubicación geográfica en la que se crearán los recursos." 
+  description = "Ubicación geográfica en la que se crearán los recursos."
 }
 
 output "name-plan" {
   value       = azurerm_service_plan.plan.name
-  description = "Nombre plan de app services" 
+  description = "Nombre plan de app services"
 }
 
 output "name-web-app" {
   value       = azurerm_linux_web_app.web-app.name
-  description = "Nombre de la aplicacion web" 
+  description = "Nombre de la aplicacion web"
 }
